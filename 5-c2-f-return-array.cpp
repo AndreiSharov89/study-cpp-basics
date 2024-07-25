e408c36937720b1251598eed5492b1aa07680408
Andrei <147848326+AndreiSharov89@users.noreply.github.com>
2024-07-25 23:53:39 +0300

    Update 5-c2-f-return-array.cpp

--- 5-c2-f-return-array.cpp
+++ 5-c2-f-return-array.cpp
@@ -6,7 +6,7 @@ int* max_vect(int kc, int* a, int* b) {
     for (int i = 0; i < kc; i++) {
         result[i] = (a[i] > b[i]) ? a[i] : b[i];
     }
-    return result; // Возвращаем указатель на созданный массив
+    return result; // Возвращаем указатель на массив
 }
 
 int main() {
@@ -14,12 +14,12 @@ int main() {
 
     int a[] = { 1, 2, 3, 4, 5, 6, 7, 2 };
     int b[] = { 7, 6, 5, 4, 3, 2, 1, 3 };
-    int kc = sizeof(a) / sizeof(a[0]); // Количество элементов массива
+    int kc = sizeof(a) / sizeof(a[0]); 
     int* c; // Указатель на результирующий массив
-    c = max_vect(kc, a, b); // Вызов функции для создания массива
-    for (int i = 0; i < kc; i++) // Вывод результата
+    c = max_vect(kc, a, b); 
+    for (int i = 0; i < kc; i++) 
         cout << c[i] << " ";
     cout << endl;
-    delete[] c; // Возврат памяти
+    delete[] c;
     return 0;
 }
