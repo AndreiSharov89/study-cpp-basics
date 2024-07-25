9d1d73f22bd7fdc1e9bf1ae43935b153862ce25d
Andrei <147848326+AndreiSharov89@users.noreply.github.com>
2024-07-25 23:54:43 +0300

    Update 5-c3-search-treanspz.cpp

--- 5-c3-search-treanspz.cpp
+++ 5-c3-search-treanspz.cpp
@@ -3,34 +3,26 @@
 using namespace std;
 
 int SearchTransposition(int arr[], int size, int key) {
-    // Если массив пустой, вернуть -1
     if (size == 0 ) {
         return -1;
     }
-
-    // Если первый элемент ключ, вернуть 0
     if (arr[0] == key) {
         return 0;
     }
-
-    // Поиск ключа в массиве
     int i;
     for (i = 1; i < size; i++) {
         if (arr[i] == key) {
-            // Если ключ найден, поменять местами текущий и предыдущий элементы
             swap(arr[i], arr[i - 1]);
             return i;
         }
     }
-
-    // Если ключ не найден, вернуть -1
     return -1;
 }
 
 int main() {
     system("chcp 1251");
 
-    int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+    int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
     int size = sizeof(arr) / sizeof(arr[0]);
 
     int index = SearchTransposition(arr, size, 5);
