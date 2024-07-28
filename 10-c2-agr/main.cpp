6e852320acabe4e0d79ee2b415cb0627f269bc6d
Andrei <147848326+AndreiSharov89@users.noreply.github.com>
2024-07-29 00:37:39 +0300

    Add files via upload
    initial

---
+++ 10-c2-agr/main.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <windows.h>
+#include "triangle.h"
+
+using namespace std;
+
+int main()
+{
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+    Dot* pDot1 = new Dot(0, 0);
+    Dot* pDot2 = new Dot(3, 0);
+    Dot* pDot3 = new Dot(0, 4);
+
+    Triangle Triangle(pDot1, pDot2, pDot3);
+    Triangle.printSides();
+    cout << "Периметр: " << Triangle.perimeter() << endl;
+    cout << "Площадь: " << Triangle.area() << endl;
+
+    delete pDot1;
+    delete pDot2;
+    delete pDot3;
+
+    return 0;
+}
+
